21555ddc9b8c674925521a312acc6e9de0d897e5
marion <the.marion.cobretti@gmail.com>
2016-05-03 19:57:48 -0400

    -Catching linuxyumrepoinstall up.  This needs to have a few corrections before its fixed.  The last block of the main needs to be looked at. --After execution is successful, it will be made into a class and imported into the linuxmaster script instead of passing things via subprocess.

--- ContentScripts/systemprep-linuxyumrepoinstall.py
+++ ContentScripts/systemprep-linuxyumrepoinstall.py
@@ -4,6 +4,8 @@ import re
 import shutil
 import sys
 import urllib2
+import json
+import logging
 
 from boto.exception import BotoClientError
 
@@ -32,22 +34,59 @@ Download the file from `url` and save it locally under `filename`.
     return True
 
 
-_supported_dists = ('amazon', 'centos', 'red hat')
-_match_supported_dist = re.compile(r'^({0})'
+def _validate_distro():
+    supported_dists = ('amazon', 'centos', 'red hat')
+
+    match_supported_dist = re.compile(r'^({0})'
                                     '(?:[^0-9]+)'
                                     '([\d]+[.][\d]+)'
                                     '(?:.*)'
-                                    .format('|'.join(_supported_dists)))
-_amazon_epel_versions = {
-    '2014.03' : '6',
-    '2014.09' : '6',
-    '2015.03' : '6',
-    '2015.09' : '6',
-}
+                                    .format('|'.join(supported_dists)))
+    amazon_epel_versions = {
+        '2014.03' : '6',
+        '2014.09' : '6',
+        '2015.03' : '6',
+        '2015.09' : '6',
+    }
 
+    # Read first line from /etc/system-release
+    release = None
+    try:
+        with open(name='/etc/system-release', mode='rb') as f:
+            release = f.readline().strip()
+    except Exception as exc:
+        raise SystemError('Could not read /etc/system-release. '
+                          'Error: {0}'.format(exc))
 
-def main(yumrepomap=None,
-         **kwargs):
+    # Search the release file for a match against _supported_dists
+    matched = match_supported_dist.search(release.lower())
+    if matched is None:
+        # Release not supported, exit with error
+        raise SystemError('Unsupported OS distribution. OS must be one of: '
+                          '{0}.'.format(', '.join(supported_dists)))
+
+    # Assign dist,version from the match groups tuple, removing any spaces
+    dist, version = (x.translate(None, ' ') for x in matched.groups())
+
+    # Determine epel_version
+    epel_version = None
+    if 'amazon' == dist:
+        epel_version = amazon_epel_versions.get(version, None)
+    else:
+        epel_version = version.split('.')[0]
+
+    if epel_version is None:
+        raise SystemError('Unsupported OS version! dist = {}, version = {}.'
+                          .format(dist, version))
+
+    return dist, version, epel_version
+
+def _yum_repo(config):
+
+    if not isinstance(config['yumrepolist'], list):
+        raise SystemError('`yumrepomap` must be a list!')
+
+def main(configuration):
     """
     Checks the distribution version and installs yum repo definition files
     that are specific to that distribution.
@@ -69,50 +108,24 @@ def main(yumrepomap=None,
                    },
                  ]
     """
-    scriptname = __file__
-    print('+' * 80)
-    print('Entering script -- {0}'.format(scriptname))
-    print('Printing parameters...')
-    print('    yumrepomap = {0}'.format(yumrepomap))
-
-    if not yumrepomap:
-        print('`yumrepomap` is empty. Nothing to do!')
-        return None
-
-    if not isinstance(yumrepomap, list):
-        raise SystemError('`yumrepomap` must be a list!')
-
-    # Read first line from /etc/system-release
-    release = None
     try:
-        with open(name='/etc/system-release', mode='rb') as f:
-            release = f.readline().strip()
-    except Exception as exc:
-        raise SystemError('Could not read /etc/system-release. '
-                          'Error: {0}'.format(exc))
+        config = json.loads(configuration)
+    except ValueError:
+        logging.fatal('The configuration passed was not properly formed JSON.  Execution Halted.')
+        sys.exit(1)
 
-    # Search the release file for a match against _supported_dists
-    m = _match_supported_dist.search(release.lower())
-    if m is None:
-        # Release not supported, exit with error
-        raise SystemError('Unsupported OS distribution. OS must be one of: '
-                          '{0}.'.format(', '.join(_supported_dists)))
+    scriptname = __file__
+    logging.info('+' * 80)
+    logging.info('Entering script -- {0}'.format(scriptname))
+    logging.info('Printing parameters...')
 
-    # Assign dist,version from the match groups tuple, removing any spaces
-    dist,version = (x.translate(None, ' ') for x in m.groups())
-
-    # Determine epel_version
-    epel_version = None
-    if 'amazon' == dist:
-        epel_version = _amazon_epel_versions.get(version, None)
+    if 'yumrepomap' in config and config['yumrepomap']:
+        dist, version, epel_version = _yum_repo(config)
     else:
-        epel_version = version.split('.')[0]
+        logging.info('yumrepomap did not exist or was empty.')
 
-    if epel_version is None:
-        raise SystemError('Unsupported OS version! dist = {0}, version = {1}.'
-                          .format(dist, version))
-
-    for repo in yumrepomap:
+    # TODO This block is weird.  Correct and done.
+    for repo in config['yumrepomap']:
         # Test whether this repo should be installed to this system
         if repo['dist'] in [dist, 'all'] and repo.get('epel_version', 'all') \
                                                 in [epel_version, 'all']:
@@ -121,8 +134,8 @@ def main(yumrepomap=None,
             repofile = '/etc/yum.repos.d/{0}'.format(url.split('/')[-1])
             download_file(url, repofile)
 
-    print('{0} complete!'.format(scriptname))
-    print('-' * 80)
+    logging.info('{0} complete!'.format(scriptname))
+    logging.info('-' * 80)
 
 
 # def _convert_string_to_list_of_dicts(fixme):
@@ -142,11 +155,12 @@ def main(yumrepomap=None,
 
 
 if __name__ == "__main__":
-    # Convert command line parameters of the form `param=value` to a dict
-    kwargs = dict(x.split('=', 1) for x in sys.argv[1:])
-    # Convert parameter keys to lowercase, parameter values are unmodified
-    kwargs = dict((k.lower(), v) for k, v in kwargs.items())
+    # # Convert command line parameters of the form `param=value` to a dict
+    # kwargs = dict(x.split('=', 1) for x in sys.argv[1:])
+    # # Convert parameter keys to lowercase, parameter values are unmodified
+    # kwargs = dict((k.lower(), v) for k, v in kwargs.items())
+    #
+    # # Need to convert a formatted string to a list of dicts,
+    # kwargs['yumrepomap'] = json.loads(kwargs.get('yumrepomap', ''))
 
-    # Need to convert a formatted string to a list of dicts,
-    kwargs['yumrepomap'] = json.loads(kwargs.get('yumrepomap', ''))
-    main(**kwargs)
+    main(sys.argv[1])
