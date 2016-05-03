454d8533dd19c802753ffde6c37df1b1e965cdda
marion <the.marion.cobretti@gmail.com>
2016-05-03 15:25:39 -0400

    -Clean Up of SystemPrep  --Added simple logging  --Added an ErrorHandler that can be replaced with a better handler.  Didnt like the blanket Exceptions and werent handling the raises.  This at least handles them and exits.  Should update the ErrorHandler and remove the blanket exceptions.  --Added YAML config file use  --Added jsonifying the dict for output.

--- MasterScripts/systemprep-linuxmaster.py
+++ MasterScripts/systemprep-linuxmaster.py
@@ -1,125 +1,172 @@
 #!/usr/bin/env python
-
+import logging
+import sys
 import argparse
-import boto, urllib2
-import os, platform, shutil, subprocess
+import boto
+import urllib2
+import os
+import platform
+import shutil
+import subprocess
+import yaml
+import json
+import datetime
+
+def ExceptionHandler(msg):
+    print(msg)
+    sys.exit()
+
 
 class SystemPrep:
-    kwargs = None
-    script_path = None
-    system = None
 
-    system_params = None
-
-    def __init__(self, params, scriptpath, system):
+    def __init__(self, params, scriptpath, config_path, stream=False):
         """
         Class constructor
         """
         self.kwargs = params
         self.script_path = scriptpath
-        self.system = system
+        self.system = platform.system()
+        self.config_path = config_path
+        # TODO create a config path and add this in so it can be automagically obtained. This will remove the hardcoded local path.
+        self.default_config = 'default.yaml'
+        self.config = None
+        self.system_params = None
+        self.system_drive = None
+        self.execution_scripts = None
+
+        logging.info('Parameters:\n{}'.format(self.kwargs))
+        logging.info('System Type:\t'.format(self.system))
+
+
+        if stream and os.path.exists('logs'):
+            logging.basicConfig(filename=os.path.join('.', 'SystemPrep-{}.log'.format(str(datetime.date.today()))),
+                                format='%(levelname)s:\t%(message)s',
+                                level=logging.DEBUG)
+            logging.info('\n\n\n{}'.format(datetime.datetime.now()))
+        else:
+            logging.warning('Stream logging is not enabled!')
+
+    def _get_config_data(self):
+
+        if not os.path.exists(self.config_path):
+            logging.warning('{} does not exist.  using the default config.'.format(self.config_path))
+            self.config_path = self.default_config
+
+        data = None
+        with open(self.config_path) as f:
+            data = f.read()
+
+        self.config = yaml.dump(data)
 
     def execute_scripts(self):
         """
         Master Script that calls content scripts to be deployed when provisioning systems.
         """
-        print('+' * 80)
-        print('Entering script -- {0}'.format(self.script_path))
-        print('Printing parameters --')
-        for key, value in self.kwargs.items():
-            print('    {0} = {1}'.format(key, value))
+        logging.info('+' * 80)
+        logging.info('Entering script -- {0}'.format(self.script_path))
 
-        print self.system
-        self.get_system_params()
-        print self.system_params
-        scripts = self.get_scripts_to_execute(self.system_params['workingdir'])
+        self._get_system_params()
+        logging.info(self.system_params)
+
+        self._get_scripts_to_execute()
+        logging.info('Got scripts to execute.')
 
         # Loop through each 'script' in scripts
-        for script in scripts:
+        for script in self.execution_scripts[self.system]:
             url = script['ScriptSource']
-            print url
+            logging.info(url)
+
             filename = url.split('/')[-1]
-            print filename
-            fullfilepath = self.system_params['workingdir'] + self.system_params['pathseparator'] + filename
-            print fullfilepath
+            logging.info(filename)
+
+            fullfilepath = os.path.join(self.system_params['workingdir'], filename)
+            logging.info(fullfilepath)
 
             # Download each script, script['ScriptSource']
             self.download_file(url, fullfilepath, self.kwargs['sourceiss3bucket'])
 
             # Execute each script, passing it the parameters in script['Parameters']
-            print('Running script -- ' + script['ScriptSource'])
-            print('Sending parameters --')
-            args = ['python', fullfilepath]
-            for key, value in script['Parameters'].items():
-                print('    {0} = {1}'.format(key, value))
-                args.append("{0}={1}".format(key, str(value).replace("'", '"')))
+            logging.info('Running script -- {}'.format(script['ScriptSource']))
+            logging.info('Sending parameters --')
+
             if 'Linux' in self.system:
-                result = subprocess.call(args)
+                result = subprocess.call(['python',
+                                          fullfilepath,
+                                          json.dumps(script['Parameters'])])
             else:
                 paramstring = ' '.join("%s='%s'" % (key, val) for (key, val) in script['Parameters'].iteritems())
                 powershell = 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe '
                 fullcommand = powershell + ' {0} {1}'.format(fullfilepath, paramstring)
                 # We need to do the same for Windows that we do for Linux, but need to test it out ....
                 result = subprocess.call(fullcommand, shell=True)
+
             if result is not 0:
                 message = 'Encountered an unrecoverable error executing a ' \
                           'content script. Exiting with failure.\n' \
                           'Command executed: {0}' \
                     .format(args)
-                raise SystemError(message)
+                ExceptionHandler(message)
 
         self.cleanup()
 
         if self.kwargs['noreboot']:
-            print('Detected `noreboot` switch. System will not be rebooted.')
+            logging.info('Detected `noreboot` switch. System will not be rebooted.')
         else:
-            print('Reboot scheduled. System will reboot after the script exits.')
+            logging.info('Reboot scheduled. System will reboot after the script exits.')
             subprocess.call(self.system_params['restart'], shell=True)
 
-        print('{0} complete!'.format(self.script_path))
-        print('-' * 80)
+        logging.info('{} complete!'.format(self.script_path))
+        logging.info('-' * 80)
 
-    def get_system_params(self):
+    def _linux_paths(self):
+        params = {}
+        params['prepdir'] = os.path.join('{}'.format(self.system_drive), 'usr', 'tmp', 'systemprep')
+        params['readyfile'] = os.path.join('{}'.format(self.system_drive), 'var', 'run', 'system-is-ready')
+        params['logdir'] = os.path.join('{}'.format(self.system_drive), 'var', 'log')
+        params['workingdir'] = '{}'.format(params['prepdir'], 'workingfiles')
+        params['restart'] = 'shutdown -r +1 &'
+        self.system_params = params
+
+    def _windows_paths(self):
+        params = {}
+        params['prepdir'] = os.path.join('{}'.format(self.system_drive), 'SystemPrep')
+        params['readyfile'] = os.path.join('{}'.format(params['prepdir']), 'system-is-ready')
+        params['logdir'] = os.path.join('{}'.format(params['prepdir']), 'Logs')
+        params['workingdir'] = os.path.join('{}'.format(params['prepdir']), 'WorkingFiles')
+        params['shutdown_path'] = os.path.join('{}'.format(os.environ['SYSTEMROOT']),'system32', 'shutdown.exe')
+        params['restart'] = params['shutdown_path']  + ' /r /t 30 /d p:2:4 /c ' + \
+                            '"SystemPrep complete. Rebooting computer."'
+        self.system_params = params
+
+    def _get_system_params(self):
         """
         Returns a dictionary of OS platform-specific parameters.
             :rtype : dict
         """
         params = {}
         if 'Linux' in self.system:
-            systemdrive = '/var'
-            params['pathseparator'] = '/'
-            params['prepdir'] = '/usr/tmp/systemprep'.format(systemdrive)
-            params['readyfile'] = '/var/run/system-is-ready'
-            params['logdir'] = '/var/log'
-            params['workingdir'] = '{0}/workingfiles'.format(params['prepdir'])
-            params['restart'] = 'shutdown -r +1 &'
+            self.systemdrive = '/'
+            self._linux_paths()
         elif 'Windows' in self.system:
-            systemdrive = os.environ['SYSTEMDRIVE']
-            params['pathseparator'] = '\\'
-            params['prepdir'] = '{0}\\SystemPrep'.format(systemdrive)
-            params['readyfile'] = '{0}\\system-is-ready'.format(params['prepdir'])
-            params['logdir'] = '{0}\\Logs'.format(params['prepdir'])
-            params['workingdir'] = '{0}\\WorkingFiles'.format(params['prepdir'])
-            params['restart'] = ('{0}\\system32\\shutdown.exe '
-                                 '/r /t 30 /d p:2:4 /c '
-                                 '"SystemPrep complete. Rebooting computer."').format(os.environ['SYSTEMROOT'])
+            self.systemdrive = os.environ['SYSTEMDRIVE']
+            self._windows_paths()
         else:
-            #TODO: Update `except` logic
-            raise SystemError('System, {0}, is not recognized?'.format(self.system))
+            logging.fatal('System, {}, is not recognized?'.format(self.system))
+            ExceptionHandler('The scripts do not recognize this system type: {}'.format(self.system))
 
         # Create SystemPrep directories
         try:
-            if not os.path.exists(params['logdir']):
-                os.makedirs(params['logdir'])
-            if not os.path.exists(params['workingdir']):
-                os.makedirs(params['workingdir'])
+            if not os.path.exists(self.system_params['logdir']):
+                os.makedirs(self.system_params['logdir'])
+            if not os.path.exists(self.system_params['workingdir']):
+                os.makedirs(self.system_params['workingdir'])
         except Exception as exc:
-            # TODO: Update `except` logic
-            raise SystemError('Could not create a directory in {0}.\n'
-                              'Exception: {1}'.format(params['prepdir'], exc))
-        self.system_params = params
+            logging.fatal('Could not create a directory in {}.\n'
+                          'Exception: {}'.format(self.system_params['prepdir'], exc))
+            ExceptionHandler(exc)
 
-    def get_scripts_to_execute(self, workingdir):
+
+    def _get_scripts_to_execute(self):
         """
         Returns an array of hashtables. Each hashtable has two keys: 'ScriptSource' and 'Parameters'.
         'ScriptSource' is the path to the script to be executed. Only supports http/s sources currently.
@@ -128,106 +175,20 @@ class SystemPrep:
             :param workingdir: str, the working directory where content should be saved
             :rtype : dict
         """
-        if 'Linux' in self.system:
-            scriptstoexecute = (
-                {
-                    'ScriptSource': "https://systemprep.s3.amazonaws.com/ContentScripts/systemprep-linuxyumrepoinstall.py",
-                    'Parameters': self.merge_dicts({
-                        'yumrepomap': [
-                            {
-                                'url': 'https://s3.amazonaws.com/systemprep-repo/linux/yum.repos/systemprep-repo-amzn.repo',
-                                'dist': 'amazon',
-                            },
-                            {
-                                'url': 'https://s3.amazonaws.com/systemprep-repo/linux/yum.repos/systemprep-repo-centos.repo',
-                                'dist': 'centos',
-                            },
-                            {
-                                'url': 'https://s3.amazonaws.com/systemprep-repo/linux/yum.repos/systemprep-repo-rhel.repo',
-                                'dist': 'redhat',
-                            },
-                            {
-                                'url': 'https://s3.amazonaws.com/systemprep-repo/linux/yum.repos/systemprep-repo-salt-el6.repo',
-                                'dist': 'all',
-                                'epel_version': '6',
-                            },
-                            {
-                                'url': 'https://s3.amazonaws.com/systemprep-repo/linux/yum.repos/systemprep-repo-salt-el7.repo',
-                                'dist': 'all',
-                                'epel_version': '7',
-                            },
-                        ],
-                    }, self.kwargs)
-                },
-                {
-                    'ScriptSource': "https://systemprep.s3.amazonaws.com/ContentScripts/SystemPrep-LinuxSaltInstall.py",
-                    'Parameters': self.merge_dicts({
-                        'saltinstallmethod': 'yum',
-                        'saltcontentsource': "https://systemprep-content.s3.amazonaws.com/linux/salt/salt-content.zip",
-                        'formulastoinclude': [
-                            "https://salt-formulas.s3.amazonaws.com/systemprep-formula-master.zip",
-                            "https://salt-formulas.s3.amazonaws.com/ash-linux-formula-master.zip",
-                            "https://salt-formulas.s3.amazonaws.com/join-domain-formula-master.zip",
-                            "https://salt-formulas.s3.amazonaws.com/scc-formula-master.zip",
-                        ],
-                        'formulaterminationstrings': [
-                            "-master",
-                            "-latest",
-                        ],
-                        'saltstates': 'Highstate',
-                        'entenv': 'False',
-                        'salt_results_log': '/var/log/saltcall.results.log',
-                        'salt_debug_log': '/var/log/saltcall.debug.log',
-                        'sourceiss3bucket': True,
-                    }, self.kwargs)
-                },
-            )
-        elif 'Windows' in self.system:
-            scriptstoexecute = (
-                {
-                    'ScriptSource': "https://systemprep.s3.amazonaws.com/SystemContent/Windows/Salt/SystemPrep-WindowsSaltInstall.ps1",
-                    'Parameters': self.merge_dicts({
-                        'saltworkingdir': '{0}\\SystemContent\\Windows\\Salt'.format(workingdir),
-                        'saltcontentsource': "https://systemprep.s3.amazonaws.com/SystemContent/Windows/Salt/salt-content.zip",
-                        'formulastoinclude': [
-                            "https://salt-formulas.s3.amazonaws.com/systemprep-formula-master.zip",
-                            "https://salt-formulas.s3.amazonaws.com/ash-windows-formula-master.zip",
-                        ],
-                        'formulaterminationstrings': [
-                            "-latest",
-                        ],
-                        'ashrole': "MemberServer",
-                        'entenv': 'False',
-                        'saltstates': "Highstate",
-                    }, self.kwargs)
-                },
-            )
-        else:
-            # TODO: Update `except` logic
-            raise SystemError('System, {0}, is not recognized?'.format(self.kwargs['system']))
+        self._get_config_data()
 
-        return scriptstoexecute
+        scriptstoexecute = self.config[self.system]
+        for item in self.config[self.system]:
+            try:
+                self.config[self.system][item]['Parameters'].update(self.kwargs)
+            except Exception as exc:
+                logging.fatal('For {} in {} the parameters could not be merged'.format(
+                    item,
+                    self.config_path
+                ))
+                ExceptionHandler(exc)
 
-    def merge_dicts(self, a, b):
-        """
-        Merge two dictionaries. If there is a key collision, `b` overrides `a`.
-            :param a: Dictionary of default settings
-            :param b: Dictionary of override settings
-            :rtype : dict
-        """
-
-        try:
-            a.update(b)
-        except Exception as exc:
-            # TODO: Update `except` logic
-            raise SystemError('Failed to merge dictionaries. Dictionary A:\n\n'
-                              '{0}\n\n'
-                              'Dictionary B:\n\n'
-                              '{1}\n\n'
-                              'Exception: {2}'
-                              .format(a, b, exc))
-
-        return a
+        self.execution_scripts = scriptstoexecute
 
     def download_file(self, url, filename, sourceiss3bucket = False):
         """
@@ -297,27 +258,30 @@ class SystemPrep:
         Removes temporary files loaded to the system.
             :return: bool
         """
-        print('+-' * 40)
-        print('Cleanup Time...')
+        logging.info('+-' * 40)
+        logging.info('Cleanup Time...')
         try:
             shutil.rmtree(self.system_params['workingdir'])
         except Exception as exc:
             # TODO: Update `except` logic
-            raise SystemError('Cleanup Failed!\n'
+            logging.fatal('Cleanup Failed!\n'
                               'Exception: {0}'.format(exc))
+            ExceptionHandler('Cleanup Failed.\nAborting.')
 
-        print('Removed temporary data in working directory -- ' + self.system_params['workingdir'])
-        print('Exiting cleanup routine...')
-        print('-+' * 40)
+        logging.info('Removed temporary data in working directory -- {}'.format(self.system_params['workingdir']))
+        logging.info('Exiting cleanup routine...')
+        logging.info('-+' * 40)
         return True
 
-if "__main__" == __name__:
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--noreboot', type=bool, default=False, choices=[True, False])
     parser.add_argument('--sourceiss3bucket', type=bool, default=False, choices=[True, False])
     params = parser.parse_known_args()
 
     kwargs = vars(params[0])
+
+
 
     # Loop through extra parameters and put into dictionary.
     for param in params[1]:
@@ -331,5 +295,5 @@ if "__main__" == __name__:
             message = 'Encountered an invalid parameter: {0}'.format(param)
             raise ValueError(message)
 
-    systemprep = SystemPrep(kwargs, os.path.abspath(parser.prog), platform.system())
+    systemprep = SystemPrep(kwargs, os.path.abspath(parser.prog), 'config.yaml', stream=True)
     systemprep.execute_scripts()
