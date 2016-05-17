44b7a639f4bffa606fa434b1a2d024015ad2e5be
marion <the.marion.cobretti@gmail.com>
2016-05-17 13:36:37 -0400

    -Correcting some naming mismatches.

--- WatchMaker/managers/workers.py
+++ WatchMaker/managers/workers.py
@@ -1,6 +1,4 @@
-import os
 import json
-import logging
 
 from watchmaker.workers.base import Salt, Yum
 from watchmaker.managers.base import WorkersManagerBase, LinuxManager, WindowsManager
@@ -24,25 +22,8 @@ class LinuxWorkersManager(WorkersManagerBase):
         pass
 
     def worker_cadence(self):
-        
-        # Loop through each 'script' in scripts
+
         for script in self.execution_scripts:
-            url = self.execution_scripts[script]['ScriptSource']
-            logging.info(url)
-
-            filename = url.split('/')[-1]
-            logging.info(filename)
-
-            fullfilepath = os.path.join(self.system_params['workingdir'], filename)
-            logging.info(fullfilepath)
-
-            # Download each script, script['ScriptSource']
-            self.manager.download_file(url, fullfilepath, self.s3)
-
-            # Execute each script, passing it the parameters in script['Parameters']
-            logging.info('Running script -- {0}'.format(self.execution_scripts[script]['ScriptSource']))
-            logging.info('Sending parameters --')
-
             configuration = json.dumps(self.execution_scripts[script]['Parameters'])
 
             if 'Yum' in script:
