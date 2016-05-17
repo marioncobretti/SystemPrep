f3f8c8c49356535aca58ccb73f84597825e78d57
marion <the.marion.cobretti@gmail.com>
2016-05-17 11:56:06 -0400

    -Including static files for default configs.

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name = 'watchmaker',
     version = '0.1',
-    packages = find_packages()
+    packages = find_packages(),
+    include_package_data = True
 )
